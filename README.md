2042244a811503f1b80d013ca0f8e7f62d86cdf0
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-01-27 18:30:23 +0530

    -Added statusbar component -added network connectivity handler -readme updated

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # RN_Messaging
+
+### Description of messaging app
+##### A simple messaging app that will let us send text, send photos from the camera roll and share our location. It will let us know that we are disconnected from the network. It will handle keyboard interactions and the back button on Android
+
+---
+
+### Application screens
+#### MessagingList Screen
+![Messaging Screen](./externalAssets/S1.png "Screen1")
+#### MessagingList with camaraRoll
+![with Photo Screen](./externalAssets/S2.png "Screen2")
+
+#### ViewPhoto Screen
+![ViewPhoto Screen](./externalAssets/S3.png "Screen3")
+---
+
+ #### Breaking down MessagingList Screen
+ ![Screen 4](./externalAssets/S4.png "MessagingList Screen Components")
+
+ ---
+
+ ### KEY POINTS
+ + API components used 
+   - **Alert** : Displays modal dialog window for simple user input
+   - **BackHnadler** : Controls the back button on Android
+   - **CameraRoll** Returns images and videos stored on the device.
+   - **Dimensions** Returns the dimensions of the screen
+   - **Geolocation** returns the location of the device and emits events when location changes.
+   - **Keyboard** Emits events when keyboard appears or disappears
+   - **NetInfo** Returns network connectivity information, and emits events when connectivity changes.
+   - **PixelRatio** Translates from density-independent pixels to density-dependent pixels.
+   - **StatusBar** Controls the visibility & color of status bar
++ **pointerEvents={'none'}** added to Status component so that the view doesnt prevent us from tapping the *ScrollView* we'll rendr it.
+The *pointerEvents* prop allows us to control whether an component can respond to touch interactions or whether they pass through to the component behind it.
+
---
+++ src/components/Status.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, StatusBar, Platform } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
+import { StatusBarHeight } from '../utils/StatusBarHeight';
+
+class Status extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: true,
+        };
+    }
+
+    async componentDidMount() {
+        this.subscription = NetInfo.addEventListener(state => {
+            console.log('Connection type', state);
+            this.setState({
+                info: state.isInternetReachable,
+            });
+        });
+        const info = await NetInfo.fetch();
+
+        // eslint-disable-next-line react/no-did-mount-set-state
+        this.setState({
+            // info: info.isConnected,
+            info: info.isInternetReachable,
+        });
+    }
+
+    componentWillUnmount() {
+        this.subscription();
+    }
+
+    render() {
+        const { info } = this.state;
+        const isConnected = info;
+        const backgroundColor = isConnected ? 'white' : 'red';
+
+        const statusBar = (
+            <StatusBar
+                backgroundColor={backgroundColor} //android only
+                barStyle={isConnected ? 'dark-content' : 'light-content'} //dark-content || light-content || default
+                animated={false}
+            />
+        );
+
+        const messageContainer = (
+            <View style={styles.messageContainer} pointerEvents={'none'}>
+                {statusBar}
+                { !isConnected &&
+                    <View style={styles.bubble}>
+                        <Text style={styles.text}>
+                            {'No network connection'}
+                        </Text>
+                    </View>
+                }
+            </View>
+        );
+
+        if (Platform.OS === 'ios') {
+            return (
+                <View style={[styles.status, { backgroundColor }]}>
+                    {messageContainer}
+                </View>
+            );
+        }
+        return messageContainer;
+    }
+}
+
+const styles = StyleSheet.create({
+    status: {
+        height: StatusBarHeight,
+        zIndex: 1,
+    },
+    messageContainer: {
+        zIndex: 1,
+        ...StyleSheet.absoluteFillObject,
+        top: StatusBarHeight + 20,
+        height: 80,
+        alignItems: 'center',
+    },
+    bubble: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        backgroundColor: 'red',
+    },
+    text: {
+        color: 'white',
+    },
+});
+
+export default Status;
